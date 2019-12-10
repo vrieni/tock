bfad133c7fb9c64e273fd0c759d628e5bd9373f5
Brad Campbell <bradjc5@gmail.com>
2019-12-10 11:44:44 -0500

    expand to full panic, seems to make apps work...

--- boards/nano33ble/src/io.rs
+++ boards/nano33ble/src/io.rs
@@ -1,14 +1,66 @@
+// use core::panic::PanicInfo;
+// use kernel::debug;
+// use kernel::hil::led;
+// use nrf52840::gpio::Pin;
+
+// /// Panic.
+// #[cfg(not(test))]
+// #[no_mangle]
+// #[panic_handler]
+// pub unsafe extern "C" fn panic_fmt(_pi: &PanicInfo) -> ! {
+//     const LED1_PIN: Pin = Pin::P0_13;
+//     let led = &mut led::LedLow::new(&mut nrf52840::gpio::PORT[LED1_PIN]);
+//     debug::panic_blink_forever(&mut [led])
+// }
+
+
+use core::fmt::Write;
 use core::panic::PanicInfo;
+use cortexm4;
 use kernel::debug;
 use kernel::hil::led;
+use kernel::hil::uart::{self, Configure};
 use nrf52840::gpio::Pin;
 
-/// Panic.
+use crate::PROCESSES;
+
+struct Writer {
+    initialized: bool,
+}
+
+static mut WRITER: Writer = Writer { initialized: false };
+
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+        let uart = unsafe { &mut nrf52840::uart::UARTE0 };
+        if !self.initialized {
+            self.initialized = true;
+            uart.configure(uart::Parameters {
+                baud_rate: 115200,
+                stop_bits: uart::StopBits::One,
+                parity: uart::Parity::None,
+                hw_flow_control: false,
+                width: uart::Width::Eight,
+            });
+        }
+        for c in s.bytes() {
+            unsafe {
+                uart.send_byte(c);
+            }
+            while !uart.tx_ready() {}
+        }
+        Ok(())
+    }
+}
+
 #[cfg(not(test))]
 #[no_mangle]
 #[panic_handler]
-pub unsafe extern "C" fn panic_fmt(_pi: &PanicInfo) -> ! {
+/// Panic handler
+pub unsafe extern "C" fn panic_fmt(pi: &PanicInfo) -> ! {
+    // The nRF52840DK LEDs (see back of board)
     const LED1_PIN: Pin = Pin::P0_13;
     let led = &mut led::LedLow::new(&mut nrf52840::gpio::PORT[LED1_PIN]);
-    debug::panic_blink_forever(&mut [led])
+    let writer = &mut WRITER;
+    debug::panic(&mut [led], writer, pi, &cortexm4::support::nop, &PROCESSES)
 }
